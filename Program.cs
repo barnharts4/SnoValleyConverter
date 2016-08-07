ef1b5be8c910db465d109155d84b868bbf78044a
barnharts4 <github@barnharts4.com>
2016-08-07 15:16:13 -0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Diagnostics;
+
+namespace SnoValleyConverter
+{
+    class Program
+    {
+        static string Convert(string ofxFile)
+        {
+            string qfxFile = ofxFile + ".qfx";
+            try 
+            {
+                using (StreamReader reader = File.OpenText(ofxFile))
+                {
+                    using (StreamWriter writer = new StreamWriter(qfxFile))
+                    {
+                        string line;
+                        while ((line = reader.ReadLine()) != null)
+                        {
+                            if (line.ToUpper().Equals("<FID>33333333"))
+                            {
+                                line = "<FID>24399";
+                            }
+                            else if (line.ToUpper().Equals("<INTU.BID>33333333"))
+                            {
+                                line = "<INTU.BID>24399";
+                            }
+                            writer.WriteLine(line);
+                        }
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(String.Format("Unable to convert {0} to {1}, error: {2}", ofxFile, qfxFile, e.Message));
+                qfxFile = null;
+            }
+
+            return qfxFile;
+        }
+
+        static void Main(string[] args)
+        {
+            if (args.Length != 1)
+            {
+                Console.WriteLine("Usage: SnoValleyConverter <OFX_File.ofx>");
+            }
+            else
+            {
+                Console.WriteLine(String.Format("Converting {0}...", args[0]));
+                string qfxPath = Convert(args[0]);
+
+                if (qfxPath != null)
+                {
+                    Process.Start(qfxPath);
+                }
+            }
+        }
+    }
+}
